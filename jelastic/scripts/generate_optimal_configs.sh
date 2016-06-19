4e2608ae682906e13aa5bc5ba39ecc7da4add8e1
Dmitry Zubelevich <dz@jelastic.com>
2016-06-19 15:00:53 +0300

    JE-26997 [Enable MaxPermSize in container if RAM is > 800MB ( for Java 6, 7)] - adjustment for cartridges

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -10,8 +10,6 @@ SED=$(which sed);
 
 CASSANDRA_ENV_FILE="${OPENSHIFT_CASSANDRA_DIR}/versions/${Version}/conf/cassandra-env.sh";
 
-memory_total=`free -m | grep Mem | awk '{print $2}'`;
-[ -z "$XMX" ] && { let "XMX=(memory_total-35)/2"; XMX="${XMX}M";  }
-[ -z "$XMS" ] && { XMS=${XMX}; }
-$SED -i "s/MAX_HEAP_SIZE=.*/MAX_HEAP_SIZE=${XMX}/" $CASSANDRA_ENV_FILE;
-$SED -i "s/MIN_HEAP_SIZE=.*/MIN_HEAP_SIZE=${XMS}/" $CASSANDRA_ENV_FILE
+$SED -i "s/MAX_HEAP_SIZE=.*/MAX_HEAP_SIZE=${XMX_VALUE}M/" $CASSANDRA_ENV_FILE;
+$SED -i "s/MIN_HEAP_SIZE=.*/MIN_HEAP_SIZE=${XMX_VALUE}M/" $CASSANDRA_ENV_FILE
+
